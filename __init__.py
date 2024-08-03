bda3aeef4e9aa3273c9f3d2861290dc01613b111
SEKiN <112916323+SEkINVR@users.noreply.github.com>
2024-08-03 09:33:19 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .animator import FullBodyAnimator
+
+NODE_CLASS_MAPPINGS = {
+    "FullBodyAnimator": FullBodyAnimator
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "FullBodyAnimator": "Full Body Animator"
+}
